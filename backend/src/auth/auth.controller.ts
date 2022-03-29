d5755e1592b0adf3b5b14dc81fa5fe06e2ad8c11
jdwns96 <jdwns96>
2022-03-29 16:18:06 +0900

    [UPDATE] nest

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { JoinDTO, LoginDTO } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
-  @Get()
-  test(): string {
-    return 'test';
+  // 로그인
+  @Post()
+  login(@Body() loginData: LoginDTO) {
+    console.log(loginData);
+    return loginData;
   }
 
-  // @Post()
-  // login() {}
-  // @Post()
-  // join() {}
+  // 회원가입
+  @Post()
+  join(@Body() joinData: JoinDTO) {
+    console.log(joinData);
+    return joinData;
+  }
 }
--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,1 +1,20 @@
-export {};
+import { IsString } from 'class-validator';
+
+export class JoinDTO {
+  @IsString()
+  readonly id: string;
+
+  @IsString()
+  readonly password: string;
+
+  @IsString()
+  readonly name: string;
+}
+
+export class LoginDTO {
+  @IsString()
+  readonly id: string;
+
+  @IsString()
+  readonly password: string;
+}
